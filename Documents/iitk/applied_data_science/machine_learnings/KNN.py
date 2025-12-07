257a20f3edf4bd2d7ea7d6d88919efea49052cfb
vaishnavi-29019 <pandeyvaishnavics16196@gmail.com>
2025-12-07 19:40:38 +0530

    add binary classification with confusion metrix

---
+++ Documents/iitk/applied_data_science/machine_learnings/KNN.py
@@ -0,0 +1,13 @@
+"""
+KNN Algorithm: K-nearest Neighborhood
+
+- supervised learning algorithm (applicable for both regression and classification)
+- sklearn: k-neighbors classifier (for classification), k-neighbors regressor (for regression)
+- distance formula: sqroot((x2-x2)^2 + (y2-y1)^2) (euclidean distance formula)
+
+
+Instant based algorithm:
+Training Process
+1. decide the value of 'K' where k belongs to N and k>0 where k is hyperparameter
+
+"""
